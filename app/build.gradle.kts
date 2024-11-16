c5eccc628b537527a77806045a5e8a219ac633d0
KGeri201 <36195747+kgerg@users.noreply.github.com>
2024-11-16 21:48:23 +0100

    updated release settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,11 +18,17 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isDebuggable = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            debug {
+                isDebuggable = true
+                isMinifyEnabled = false
+            }
         }
     }
     compileOptions {
